c3986904b1e5474d51603982d9f7263b0509237c
tinahmbabazi <christine.ainembabazi.ent.2020@gmail.com>
2026-05-16 23:49:58 +0300

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,7 @@
+function showAlert() {
+    alert('Thank you for contacting us! We will get back to you soon.');
+}
+
+function changeColor(){
+ document.getElementById("about").style.color = "lightblue";
+}
